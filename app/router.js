13d5a2870246ae8801f79392d1a55eb55475e930
Jordan Hawker <hawker.jordan@gmail.com>
2015-03-02 17:40:01 -0800

    Add basic routes & navigation

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('contact');
+  this.route('music');
+  this.route('dance');
+  this.route('code');
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -16,6 +16,12 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    sassOptions: {
+      includePaths: [
+        'bower_components/bootstrap-sass-official/assets/stylesheets'
+      ]
     }
   };
 
