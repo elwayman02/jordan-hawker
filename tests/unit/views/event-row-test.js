039b3656f8cbf4f99f429751ac84ab8346e330be
Jordan Hawker <hawker.jordan@gmail.com>
2015-03-04 14:28:00 -0800

    Add tests

---
+++ tests/unit/views/event-row-test.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+var view;
+var styles = ['Blues', 'Swing', 'Fusion'];
+var dates = ['2012', '2013', '2014'];
+
+moduleFor('view:event-row', {
+  setup: function () {
+    view = this.subject();
+    view.set('content', {
+      styles: Ember.A(styles),
+      dates: Ember.A(dates)
+    });
+  }
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  assert.ok(view);
+});
+
+test('styleList', function () {
+  equal(view.get('styleList'), 'Blues, Fusion, Swing', 'sorted and concatenated by default');
+
+  var sList = view.get('content.styles').pushObject('Lindy');
+  equal(view.get('styleList'), 'Blues, Fusion, Lindy, Swing', 're-sorts when the array changes');
+});
+
+test('dateList', function () {
+  equal(view.get('dateList'), '2012, 2013, 2014', 'concatenates dates');
+});
