de0d98f33eee9d60eb4a52d9256dd43a46895120
Jordan Hawker <hawker.jordan@gmail.com>
2015-03-08 14:32:46 -0700

    Add scrolling navbar
    - Fix rimraf/glob dependencies for reload bug

--- app/views/navbar.js
+++ app/views/navbar.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  templateName: 'navbar'
+  templateName: 'navbar',
+
+  actions: {
+      click: function () {
+          // Will break of sub-menus are added; add the action to link-tos instead
+          // https://github.com/twbs/bootstrap/issues/12852#issuecomment-59621810
+          this.$('.navbar-collapse.in').collapse('hide');
+      }
+  }
 });
