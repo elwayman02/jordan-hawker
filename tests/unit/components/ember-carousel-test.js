241cc5967b0edb1c356e85a035096756cfe3e63d
Jordan Hawker <hawker.jordan@gmail.com>
2015-03-26 22:48:45 -0700

    Fix tests

--- tests/unit/components/ember-carousel-test.js
+++ tests/unit/components/ember-carousel-test.js
@@ -3,16 +3,22 @@ import {
   test
 } from 'ember-qunit';
 
+var component;
+
 moduleForComponent('ember-carousel', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
+  needs: ['component:ember-carousel-item'],
+  setup: function () {
+      component = this.subject();
+      component.set('content', []);
+  }
 });
 
 test('it renders', function(assert) {
   assert.expect(2);
 
   // creates the component instance
-  var component = this.subject();
   assert.equal(component._state, 'preRender');
 
   // renders the component to the page
