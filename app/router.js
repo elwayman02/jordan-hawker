4dce53d49fefce94ea2ff23527547240968ea400
Jordan Hawker <hawker.jordan@gmail.com>
2015-04-09 22:27:04 -0700

    Upgrade packages
    Upgrade ember-cli, ember
    Add ember-try and ember-watson
    Minor template changes

--- app/router.js
+++ app/router.js
@@ -5,11 +5,9 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
   this.route('contact');
   this.route('music');
   this.route('dance');
   this.route('code');
 });
-
-export default Router;
