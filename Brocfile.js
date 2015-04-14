51b1737c95cacfac010d8d0000c2a1c67a77710d
Jordan Hawker <hawker.jordan@gmail.com>
2015-04-14 16:09:51 -0700

    Upgrade ember-cli-sass & ember-data-model-fragments

--- Brocfile.js
+++ Brocfile.js
@@ -6,7 +6,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
   name: require('./package.json').name,
-  getEnvJSON: require('./config/environment')
+  getEnvJSON: require('./config/environment'),
+  sassOptions: {
+      includePaths: [
+          'bower_components/bootstrap-sass-official/assets/stylesheets'
+      ]
+  }
 });
 
 var fontTree = pickFiles('bower_components/fontawesome/fonts', {
