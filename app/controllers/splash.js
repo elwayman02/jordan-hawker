db3a15eeeabc5d6a39f4509769c2d019f7849eaa
Jordan Hawker <hawker.jordan@gmail.com>
2015-05-17 17:08:45 -0700

    Refactor

--- app/controllers/splash.js
+++ app/controllers/splash.js
@@ -4,7 +4,6 @@ import config from '../config/environment';
 export default Ember.Controller.extend({
     version: Ember.computed(function () {
         var version = config.APP.version;
-        version = version.substring(0, version.lastIndexOf('.'));
-        return 'Version %@'.fmt(version);
+        return 'Version %@'.fmt(version.substring(0, version.lastIndexOf('.'))); // Remove SHA from version
     })
 });
