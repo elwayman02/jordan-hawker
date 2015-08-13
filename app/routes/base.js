ddf83b32f4dd74bc7dd96e9d3ee70c7641df6ef6
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-12 23:57:23 -0700

    Replace Brocfile with ember-cli-build Update ember-intl

--- app/routes/base.js
+++ app/routes/base.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    intl: Ember.inject.service(),
+    beforeModel() {
+        this.get('intl').setLocale('en-us');
+    },
+
     activate() {
         Ember.$('html').velocity('scroll', { offset: 0, duration: 150 });
     }
