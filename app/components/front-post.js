d56286a6d32ae311ffdd788de9db88b0b0ee1ed9
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-16 00:58:05 -0700

    Add missing files

---
+++ app/components/front-post.js
@@ -0,0 +1,11 @@
+import TumblrPostText from 'ember-tumblr/components/tumblr-post-text';
+
+export default TumblrPostText.extend({
+    layoutName: 'components/tumblr-post-text',
+    expandAction: 'viewBlog',
+    actions: {
+        expand() {
+            this.sendAction('expandAction');
+        }
+    }
+});
