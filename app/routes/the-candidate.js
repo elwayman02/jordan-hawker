0fac77cd20a3698446c52956acc8235766a51955
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-16 11:24:38 -0700

    Fix API call for The-Candidate route

--- app/routes/the-candidate.js
+++ app/routes/the-candidate.js
@@ -2,6 +2,6 @@ import BaseRoute from 'jordan-hawker/routes/base';
 
 export default BaseRoute.extend({
     model() {
-        return this.store.find('post', { tag: 'TheCandidate' });
+        return this.store.find('tumblr-post-text', { tag: 'TheCandidate' });
     }
 });
