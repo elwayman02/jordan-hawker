b48e212b2eeb2b146b0186e8138144a6da122948
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-16 02:45:04 -0700

    Fix bad API call Upgrade Ember-Tumblr version

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import BaseRoute from 'jordan-hawker/routes/base';
 
 export default BaseRoute.extend({
     model() {
-        return this.store.find('post', {
+        return this.store.find('tumblr-post-text', {
             limit: 1
         }).then(function (posts) {
             return posts.get('firstObject');
