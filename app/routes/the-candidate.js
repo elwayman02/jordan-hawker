d2b95ea107d5f9273abe50d831ec6c1f31badac6
Jordan Hawker <hawker.jordan@gmail.com>
2015-09-01 21:50:58 -0700

    Update Ember-Tumblr to fix dates in all browsers Fix tag filtering with post workaround

--- app/routes/the-candidate.js
+++ app/routes/the-candidate.js
@@ -2,6 +2,6 @@ import BaseRoute from 'jordan-hawker/routes/base';
 
 export default BaseRoute.extend({
     model() {
-        return this.store.find('tumblr-post-text', { tag: 'TheCandidate' });
+        return this.store.find('tumblr-post', { tag: 'TheCandidate' });
     }
 });
