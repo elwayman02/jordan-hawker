b465c67bdf2916483905408609ce01222990d3f9
Jordan Hawker <hawker.jordan@gmail.com>
2018-02-17 00:59:31 -0800

    Add Twitch embed & toggle

---
+++ app/components/twitch-embed.js
@@ -0,0 +1,34 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  localClassNames: 'twitch-embed',
+
+  username: 'axlehellfire',
+
+  chatUrl: computed('username', function () {
+    return `http://www.twitch.tv/embed/${this.get('username')}/chat`;
+  }),
+
+  videoUrl: computed('username', function () {
+    return `http://player.twitch.tv/?channel=${this.get('username')}`;
+  }),
+
+  axleButtonClass: computed('username', function () {
+    if (this.get('username') === 'axlehellfire') {
+      return 'active';
+    }
+  }),
+
+  tiltedButtonClass: computed('username', function () {
+    if (this.get('username') === 'boardgames') {
+      return 'active';
+    }
+  }),
+
+  actions: {
+    toggle(username) {
+      this.set('username', username);
+    }
+  }
+});
