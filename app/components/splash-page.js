465ed56ff3fff254d267361cb94c4429c6a968a2
Jordan Hawker <hawker.jordan@gmail.com>
2018-02-19 12:08:57 -0800

    Instrument About Page

--- app/components/splash-page.js
+++ app/components/splash-page.js
@@ -1,12 +1,19 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { run } from '@ember/runloop';
+import ComponentInteractivity from 'ember-interactivity/mixins/component-interactivity';
 import config from '../config/environment';
 
-export default Component.extend({
+export default Component.extend(ComponentInteractivity, {
   localClassNames: 'splash-page',
 
   version: computed(function () {
     let version = config.APP.version;
     return `Version ${version.substring(0, version.lastIndexOf('.'))}`; // Remove SHA from version
-  })
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+    run.scheduleOnce('afterRender', this, this.reportInteractive);
+  }
 });
