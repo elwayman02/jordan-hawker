6cb997cfb0cf401842030804b2b447986b01fae2
Jordan Hawker <hawker.jordan@gmail.com>
2018-02-23 20:59:57 -0800

    Add mobile header nav

---
+++ app/components/header-nav-link.js
@@ -0,0 +1,5 @@
+import LeftNavLink from 'jordan-hawker/components/left-nav-link';
+
+export default LeftNavLink.extend({
+  localClassNames: 'header-nav-link'
+});
---
+++ app/components/header-nav.js
@@ -0,0 +1,5 @@
+import HeaderBar from 'jordan-hawker/components/header-bar';
+
+export default HeaderBar.extend({
+  localClassNames: 'header-nav'
+});
