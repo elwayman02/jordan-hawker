38911d693811bb602d832ff1124b56865cff6067
Jordan Hawker <hawker.jordan@gmail.com>
2018-02-23 22:38:41 -0800

    Add "landscape" notice to Twitch page for portrait mode on mobile

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -1,4 +1,5 @@
 export default {
+  mini: '(max-width: 450px)',
   mobile:  '(max-width: 825px)',
   tablet:  '(min-width: 826px) and (max-width: 991px)',
   desktop: '(min-width: 992px) and (max-width: 1200px)'
