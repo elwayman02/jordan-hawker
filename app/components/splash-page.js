7331abda527189de0f2150052f51c80db58a0721
Jordan Hawker <hawker.jordan@gmail.com>
2018-03-13 09:56:11 -0700

    Use app-version correctly
    Update to interactivity v1.0

--- app/components/splash-page.js
+++ app/components/splash-page.js
@@ -1,16 +1,9 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import { run } from '@ember/runloop';
 import ComponentInteractivity from 'ember-interactivity/mixins/component-interactivity';
-import config from '../config/environment';
 
 export default Component.extend(ComponentInteractivity, {
   localClassNames: 'splash-page',
-
-  version: computed(function () {
-    let version = config.APP.version;
-    return `Version ${version.substring(0, version.lastIndexOf('.'))}`; // Remove SHA from version
-  }),
 
   didInsertElement() {
     this._super(...arguments);
